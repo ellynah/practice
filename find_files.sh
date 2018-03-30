5542f23b7f13a5329d108dba74b5a2d719d20451
Elina Akhmanova <eakhmanova@griddynamics.com>
2018-03-30 16:20:09 +0400

    Change directory for find_files.sh

--- find_files.sh
+++ find_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Get files from 10 to 100 kB and will sort them by size in ascending order.
-find .. -type f -size +10c -a -size -100c -exec du -a {} + | sort -n
+find . -type f -size +10c -a -size -100c -exec du -a {} + | sort -n
 
